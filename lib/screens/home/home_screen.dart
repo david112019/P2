61218c6bf3ca68cb9c5d0ba3a1f4e24f831a57d8
david <david.cm3030@gmail.com>
2020-07-05 12:46:36 -0300

    Ref #7 Menu de filtro

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:xlo/blocs/home_bloc.dart';
 import 'package:xlo/commom/custom_drawer/custom_drawer.dart';
 import 'package:xlo/screens/home/widgets/search_dialog.dart';
+import 'package:xlo/screens/home/widgets/top_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -37,6 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       appBar: AppBar(
+        elevation: 0,
         title: StreamBuilder<String>(
           stream: _homeBloc.outSearch,
           initialData: 'MARKET EASY',
@@ -83,6 +85,11 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       drawer: CustomDrawer(),
+      body: Column(
+        children: <Widget>[
+          TopBar(),
+        ],
+      ),
     );
   }
 
