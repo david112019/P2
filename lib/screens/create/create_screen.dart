b45510ddcea897a1e238ceabea13c8424e4034ed
david <david.cm3030@gmail.com>
2020-07-06 20:39:57 -0300

    Ref #29 #30 Campo de anuncio]

--- lib/screens/create/create_screen.dart
+++ lib/screens/create/create_screen.dart
@@ -1,7 +1,8 @@
+import 'package:brasil_fields/formatter/real_input_formatter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:xlo/commom/custom_drawer/custom_drawer.dart';
 import 'package:xlo/screens/create/widgets/images_field.dart';
-
 
 class CreateScreen extends StatefulWidget {
   @override
@@ -25,8 +26,97 @@ class _CreateScreenState extends State<CreateScreen> {
         child: ListView(
           children: <Widget>[
             ImagesField(
+              onSaved: (images){
 
+              },
+              initialValue: [],
             ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Título *',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  color: Colors.grey[700],
+                  fontSize: 18,
+                ),
+                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+              ),
+              validator: (text) {
+                if (text.isEmpty) return 'Campo obrigatório';
+                return null;
+              },
+              onSaved: (t) {
+
+              },
+            ),
+            TextFormField(
+              maxLines: null,
+              decoration: InputDecoration(
+                labelText: 'Descrição *',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  color: Colors.grey[700],
+                  fontSize: 18,
+                ),
+                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+              ),
+              validator: (text) {
+                if (text.trim().isEmpty) return 'Campo obrigatório';
+                if (text.trim().length < 10)
+                  return 'Descrição muito curta';
+                return null;
+              },
+              onSaved: (d) {
+
+              },
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Preço *',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.w800,
+                  color: Colors.grey[700],
+                  fontSize: 18,
+                ),
+                contentPadding: const EdgeInsets.fromLTRB(16, 10, 12, 10),
+              ),
+              keyboardType: const TextInputType.numberWithOptions(
+                decimal: true,
+                signed: false,
+              ),
+              inputFormatters: [
+                WhitelistingTextInputFormatter.digitsOnly,
+                RealInputFormatter(centavos: true),
+              ],
+              validator: (text) {
+                if (text.isEmpty) return 'Campo obrigatório';
+                if (double.tryParse(text) == null)
+                  return 'Utilize valores válidos';
+                return null;
+              },
+              onSaved: (price) {
+
+              },
+            ),
+            Container(
+              height: 50,
+              child: RaisedButton(
+                color: Colors.deepPurple,
+                child: Text(
+                  'Enviar',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                onPressed: (){
+                  if(_formKey.currentState.validate()){
+                    _formKey.currentState.save();
+                  }
+                },
+              ),
+            )
           ],
         ),
       ),
